bdf58592c3b74f7e1cdd2241d151f570ef7cb58c
Cananuzma <97678285+Cananuzma@users.noreply.github.com>
2022-12-07 14:56:29 +0300

    Update mainContentText.vue
    update mainContentText

--- components/mainContentText.vue
+++ components/mainContentText.vue
@@ -1,3 +1,76 @@
 <template>
+    <div class="mainContentText-div">
+        <ul class="mainContentText-ul">
+            <li>
+                <form class="mainContentText-form">
+                    <h2 class="mainContentText-h2">Athletic Shoes</h2>
+                </form>
+            </li>
+            <li>
+                <h2 class="mainContentText-See-all">See all</h2>
+            </li>
+        </ul>
+        <ul class="mainContentText-ul2">
+            <h2 class="mainContentText-recommended">Recommended for you</h2>
+        </ul>
+    </div>
+    </template>
     
-</template>
+    <style>
+        .mainContentText-div{
+            padding: 0px;
+            margin: 0px;
+            margin-left: 15px;
+            height: 100px;
+        }
+    
+        .mainContentText-ul{
+            font-size: 1.5rem;
+            display: flex;
+            font-family: "Market Sans", Arial, sans-serif;
+            padding: 0px;
+            margin: 0px;
+            margin-bottom: 10px;
+            height: 32px;
+        }
+    
+        .mainContentText-ul2{
+            display: flex;
+            font-family: "Market Sans", Arial, sans-serif;
+            padding: 0px;
+            margin: 0px;
+            margin-left: 5px;
+            height: 20px;
+        }
+    
+        .mainContentText-recommended{
+            font-size: 16px;
+            color: #767676;
+            display: flex;
+            font-family:"Market Sans", Arial, sans-serif;
+        }
+    
+        .mainContentText-h2{
+            font-size: 1.5rem;
+            font-family: "Market Sans", Arial, sans-serif;
+            
+            
+        }
+    
+        .mainContentText-See-all{
+            font-size: 1.3rem;
+            font-family: "Market Sans", Arial, sans-serif;
+            margin-top: 20px;
+        }
+        .mainContentText-form{
+            border-right: 0.5px solid rgb(240, 240, 240);
+            width: 190px;
+            margin: 0px;
+            padding: 0px;
+        }
+    
+    </style>
+    
+    <script>
+    
+    </script>
