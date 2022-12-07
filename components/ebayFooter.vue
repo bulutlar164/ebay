6770c8a9442e9ddd02741b21392d94e8cbd5dc49
Cananuzma <97678285+Cananuzma@users.noreply.github.com>
2022-12-07 14:33:05 +0300

    Update ebayFooter.vue
    ebayfooter yapıldı

--- components/ebayFooter.vue
+++ components/ebayFooter.vue
@@ -1,3 +1,132 @@
 <template>
+    <div class="footer-div">
+        <ul class="footer-ul">
+            <li>
+                <h1 class="footer-baslık">Buy</h1>
+                <ul class="footer-content">
+                    <li>Registration</li>
+                    <li>eBay Money Back Guarantee</li>
+                    <li>Bidding & buying help</li>
+                    <li>Stores</li>
+                </ul>
+            </li>
+            <li>
+                <ul>
+                    <li class="footer-li">
+                        <h1 class="footer-baslık">Sell</h1>
+                            <ul class="footer-content">
+                                <li>Start selling</li>
+                                <li>Learn to sell</li>
+                                <li>Affiliates</li>
+                            </ul>
+                    </li>
+                    <li class="footer-li">
+                        <h1 class="footer-baslık">Tolls & apps</h1>
+                            <ul class="footer-content">
+                                <li>Developers</li>
+                                <li>Security center</li>
+                                <li>Site map</li>
+                            </ul>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <h1 class="footer-baslık">Stay connected</h1>
+                <ul class="footer-content">
+                    <li>eBay's Blogs</li>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                </ul>
+            </li>
+            <li>
+                <h1 class="footer-baslık">About eBay</h1>
+                <ul class="footer-content">
+                    <li>Company info</li>
+                    <li>News</li>
+                    <li>Investors</li>
+                    <li>Careers</li>
+                    <li>Goverment relations</li>
+                    <li>Advertise with us</li>
+                    <li>Policies</li>
+                    <li>Verified Rights Owner (VeRO) Program</li>
+                </ul>
+            </li>
+            <li>
+                <ul>
+                    <li class="footer-li">
+                        <h1 class="footer-baslık">Help & Contact</h1>
+                            <ul class="footer-content">
+                                <li>Seller Information Center</li>
+                                <li>Contact us</li>
+                            </ul>
+                    </li>
+                    <li class="footer-li">
+                        <h1 class="footer-baslık">Community</h1>
+                            <ul class="footer-content">
+                                <li>Announcements</li>
+                                <li>Discussion boards</li>
+                                <li>eBay Giving Works</li>
+                            </ul>
+                    </li>
+                    <li class="footer-li">
+                        <h1 class="footer-baslık">eBay Sites</h1>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <h1 class="footer-div-h1">Copyright © 1995-2022 eBay Inc. All Rights Reserved. Accessibility, User Agreement, Privacy, Payments Terms of Use, Cookies, Do not sell my personal information and AdChoice</h1>
+    </div>
+    </template>
     
-</template>
+    <style>
+    
+     .footer-ul{
+        display: flex;
+        height: 350px;
+     }
+    
+     .footer-ul li {
+        width: 220px;
+        margin-right: 40px;
+     }
+    
+     .footer-div{
+        height: 450px;
+        margin-top: 50px;
+     }
+    
+     .footer-div-h1{
+        font-size: 12px;
+        color: #333;
+        font-family: "Market Sans", Arial, sans-serif;
+     }
+     .footer-content{
+        margin: 0px;
+        
+     }
+    
+     .footer-li{
+        margin-bottom: 20px;
+     }
+    
+     .footer-content li{
+        font-size: 12px;
+        color: #333;
+        margin-left: 0px;
+        font-family: "Market Sans", Arial, sans-serif;
+        margin-bottom: 10px;
+     }
+    
+     .footer-baslık{
+        font-size: 16px;
+        color: #767676;
+        font-family: "Market Sans", Arial, sans-serif;
+        margin-bottom: 15px;
+     }
+    
+    
+    </style>
+    
+    <script>
+    
+    </script>
