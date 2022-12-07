b0268e816ac81b0382f5373cdd0bfbf005a8f8cd
Cananuzma <97678285+Cananuzma@users.noreply.github.com>
2022-12-07 15:20:38 +0300

    Update mainContentDailySliderText.vue
    update mainContentDailySliderText

--- components/mainContentDailySliderText.vue
+++ components/mainContentDailySliderText.vue
@@ -1,3 +1,52 @@
 <template>
+    <div class="mainContentCategoriesText-div">
+        <ul class="mainContentText-ul">
+            <li>
+                <form class="mainContentDailySliderText-form">
+                    <h2 class="mainContentText-h2">Daily Deals</h2>
+                </form>
+            </li>
+            <li>
+                <h2 class="mainContentText-See-all">See all</h2>
+            </li>
+        </ul>
+    </div>
+    </template>
     
-</template>
+    <style>
+        .mainContentCategoriesText-div{
+            padding: 0px;
+            margin: 0px;
+            margin-left: 15px;
+            height: 100px;
+        }
+
+        .mainContentText-ul{
+            font-size: 1.5rem;
+            display: flex;
+            font-family: "Market Sans", Arial, sans-serif;
+            padding: 0px;
+            margin: 0px;
+            margin-bottom: 10px;
+            height: 32px;
+        }
+
+        .mainContentText-h2{
+            font-size: 1.5rem;
+            font-family: "Market Sans", Arial, sans-serif;
+        }
+    
+        .mainContentText-See-all{
+            font-size: 1.3rem;
+            font-family: "Market Sans", Arial, sans-serif;
+            margin-top: 20px;
+        }
+        
+        .mainContentDailySliderText-form{
+            border-right: 0.5px solid rgb(240, 240, 240);
+            width: 150px;
+            margin: 0px;
+            padding: 0px;
+        }
+    
+    </style>
