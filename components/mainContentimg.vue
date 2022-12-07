b315ea329f8b544eb31e9689b2ab0e1ffa88d7b1
bulutlar164 <80857538+bulutlar164@users.noreply.github.com>
2022-12-07 14:52:10 +0300

    Update mainContentimg.vue
    uptade ContentMainImg

--- components/mainContentimg.vue
+++ components/mainContentimg.vue
@@ -1,3 +1,74 @@
 <template>
+    <div class="mainContentimg-div">
+        <img class="mainContentimg-img" src="../image/katar.png"/>
+        <div class="mainContentimg-div-div">
+            <h1 class="mainContentimg-h1">Soccer euphoria is here</h1>
+            <h1 class="mainContentimg-text">Show your passion and take amazing collectibles with you</h1>
+            <form class="mainContentimg-form">
+                <h1 class="mainContentimg-button">Go now!</h1>
+            </form>
+        </div>
+    </div>
+    </template>
     
-</template>
+    <style>
+        .mainContentimg-form
+        {
+            margin-top: 30px;
+            width: 140px;
+            height: 40px;
+            color: black;
+            border: 1px solid #333;
+        }
+    
+        .mainContentimg-button{
+            font-size: 16px;
+            font-family: "Market Sans", Arial, sans-serif;
+            margin-left: 20px;
+            color: #333;
+        }
+    
+        .mainContentimg-div{
+            position: relative;
+            width: 100%;
+            max-width: 1248px;
+    
+        }
+        .mainContentimg-img{
+            height: 330px;
+            width: 1312px;
+    
+        }
+    
+        .mainContentimg-h1{
+            font-size: 2.30rem;
+            font-family: "Market Sans", Arial, sans-serif;
+            margin: 0px;
+            padding: 0px;
+        }
+    
+        .mainContentimg-text{
+            font-size: 1.0625rem;
+            font-family: "Market Sans", Arial, sans-serif;
+            margin: 0px;
+            margin-top: 10px;
+            padding: 0px;
+            color: #555;
+        }
+    
+        .mainContentimg-div-div{
+            width: 390px;
+            position: absolute;
+            margin: 0px;
+            padding: 0px;
+            top: 4%;
+            left: 2%;
+        }
+    
+    
+    </style>
+    
+    <script>
+    
+    
+    </script>
