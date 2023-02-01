b69b93e83f2ee27bf46fb5d705a0c617948d85c0
Cananuzma <97678285+Cananuzma@users.noreply.github.com>
2023-02-01 21:37:32 +0300

    layout eklenmesi
    layout eklenmesi

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <headerDiv/>
+        <hr />
+        <headerTable/>
+        <hr/>
+      <slot/>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import headerDiv from "/components/headerDiv.vue"
+  import headerTable from "/components/headerTable.vue"
+  </script>
+  
+  <style>
+  hr{
+  margin-left:-100px;
+  }
+  
+  .app-hr{
+    margin-top: 80px;
+  }
+  </style>
+  
